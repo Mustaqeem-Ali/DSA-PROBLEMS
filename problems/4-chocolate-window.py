c4393696af2dcf74e291ffdce6c5a7fe5ed3e96e
MUSTAQEEM ALI <150370499+ali-bhai-2004@users.noreply.github.com>
2024-11-12 23:57:23 +0530

    Auto-commit: Updated files

--- problems/4-chocolate-window.py
+++ problems/4-chocolate-window.py
@@ -12,3 +12,16 @@
 # Input: arr[] = {7, 3, 2, 4, 9, 12, 56}, m = 5 
 # Output: 7
 # Explanation: If we distribute chocolate packets {3, 2, 4, 9, 7}, we will get the minimum difference, that is 9 – 2 = 7. 
+
+arr = [7, 3, 2, 4, 9, 12, 56]
+m = 5
+
+n = len(arr)
+min_diff = int('inf')
+for i in range(n - m + 1): #here last 4 elements cant iterated because fixed size of 5
+    diff = arr[i+m-1] - arr[i]
+    
+    if diff < min_diff:
+        min_diff = diff
+print(min_diff)
+    
